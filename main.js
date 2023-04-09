06548ef4afe097823e0a00a53444356bf07cbdb8
Safiyyah Mahmud Abdullah <90573177+safiyyah-tech@users.noreply.github.com>
2023-04-09 03:32:55 -0700

    interactive ratings

---
+++ main.js
@@ -0,0 +1,11 @@
+const ratingNumber = document.querySelectorAll(".number-list");
+const submitBtn = document.getElementsByClassName("button");
+const textRate = document.getElementById("rating-text");
+const startBtn = document.getElementById("startButton");
+
+// Add the event listener to the rating number
+for (let i = 0; i < ratingNumber.length; i++) {
+    const el = ratingNumber[i];
+    
+    el.addEventListener('click', function ())
+}
