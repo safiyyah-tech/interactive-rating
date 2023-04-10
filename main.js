6b778f19b460161d738c16a4f31b50f83c76a124
Toma John Musa <tomajohn316@yahoo.com>
2023-04-10 21:38:53 +0100

    Add comments instructions

--- main.js
+++ main.js
@@ -7,5 +7,13 @@ const startBtn = document.getElementById("startButton");
 for (let i = 0; i < ratingNumber.length; i++) {
     const el = ratingNumber[i];
     
-    el.addEventListener('click', function ())
+    el.addEventListener('click', function () {
+        // add active class to the div clixkeed so that user can see which div they clicked.
+        // Create a variable outside this for loop and set it to 0 initially. Set it to the number on the div that is clicked here 
+    });
 }
+
+submitBtn.addEventListener('click', () => {
+    // submits the number and displays a message thank you for rating us 
+    // You can save rating in localStorage
+})
